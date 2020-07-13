8c9c5afb27abcbb31d6970dbb4c45a5661efa02a
three4c <three4c@gmail.com>
2020-07-13 09:38:15 +0900

    create bottomSheet

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 const BottomSheet = () => {
-  return <View>hoge</View>;
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "#000", fontSize: 16 }}>Bottom Sheet Text</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default BottomSheet;
