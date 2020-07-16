5cbcd4b06512b6ce41b5a9167e19c85f94e8a91f
three4c <three4c@gmail.com>
2020-07-16 09:19:01 +0900

    add panGestureHandlerStateChangeEvent

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,8 +1,44 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Animated } from "react-native";
+import {
+  PanGestureHandler,
+  PanGestureHandlerStateChangeEvent,
+  State,
+} from "react-native-gesture-handler";
 
 const BottomSheet: React.FC = (props) => {
-  return <View style={styles.container}>{props.children}</View>;
+  const translateY = new Animated.Value(0);
+
+  const onGestureEvent = Animated.event([
+    {
+      nativeEvent: {
+        translationY: translateY,
+      },
+    },
+  ]);
+
+  const onHandlerStateChange = (event: PanGestureHandlerStateChangeEvent) => {
+    if (event.nativeEvent.oldState == State.ACTIVE) {
+      Animated.timing(translateY, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
+  return (
+    <PanGestureHandler
+      onGestureEvent={onGestureEvent}
+      onHandlerStateChange={onHandlerStateChange}
+    >
+      <Animated.View
+        style={[styles.container, { transform: [{ translateY: translateY }] }]}
+      >
+        {props.children}
+      </Animated.View>
+    </PanGestureHandler>
+  );
 };
 
 const styles = StyleSheet.create({
