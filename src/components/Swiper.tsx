94aa77f3136d9123f7fca905a74ce4190ddcf1f7
three4c <three4c@gmail.com>
2020-07-22 09:49:28 +0900

    not swipe direction

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -1,25 +1,63 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import Animated, {
+  add,
+  clockRunning,
+  cond,
+  debug,
+  divide,
+  eq,
+  floor,
+  not,
+  set,
+  useCode,
+} from "react-native-reanimated";
+import {
+  snapPoint,
+  timing,
+  useClock,
+  usePanGestureHandler,
+  useValue,
+  withOffset,
+} from "react-native-redash";
 
 const Swiper: React.FC = (props) => {
+  const {
+    gestureHandler,
+    state,
+    velocity,
+    translation,
+  } = usePanGestureHandler();
+
+  const translateX = withOffset(translation.x, state);
   return (
     <View style={styles.container}>
-      {React.Children.map(props.children, (child, index) => (
-        <View key={index} style={styles.swiperItem}>
-          {child}
-        </View>
-      ))}
+      <PanGestureHandler {...gestureHandler}>
+        <Animated.View
+          style={[styles.swiperList, { transform: [{ translateX }] }]}
+        >
+          {React.Children.map(props.children, (child, index) => (
+            <View key={index} style={styles.swiperItem}>
+              {child}
+            </View>
+          ))}
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
     flex: 1,
   },
+  swiperList: {
+    flexDirection: "row",
+    width: "100%",
+    height: "100%",
+  },
   swiperItem: {
-    padding: 24,
     width: "100%",
     height: "100%",
   },
