de0894ddfbf28a6d3e172514a4bde28065f2051c
three4c <three4c@gmail.com>
2020-08-24 09:34:14 +0900

    firebaseの導入

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
+import React from "react";
 import LoginScreen from "./src/screen/LoginScreen";
 import SignupScreen from "./src/screen/SignupScreen";
 import MainScreen from "./src/screen/MainScreen";
 import AddTaskScreen from "./src/screen/AddTaskScreen";
 import EditTaskScreen from "./src/screen/EditTaskScreen";
 
-import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
+import firebase from "firebase";
+import ENV from "./env.json";
+
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_SENDER_ID,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const MainStack = createStackNavigator();
 const RootStack = createStackNavigator();
